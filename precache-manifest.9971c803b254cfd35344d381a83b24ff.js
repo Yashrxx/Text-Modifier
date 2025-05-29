31d0c62377010c7f1f294ed81eca3237d571de16
Yashrxx <yashkumar.s2023@vitstudent.ac.in>
2025-05-30 01:33:13 +0530

    Updates

---
+++ precache-manifest.9971c803b254cfd35344d381a83b24ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab7a88ba3a2980806ad3c85a6fd2aca0",
+    "url": "/Text-Modifier/index.html"
+  },
+  {
+    "revision": "c4fda1f42d1186726c67",
+    "url": "/Text-Modifier/static/css/main.ae24c6cf.chunk.css"
+  },
+  {
+    "revision": "9db7eea78314db4e6ff3",
+    "url": "/Text-Modifier/static/js/2.dee8e078.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/Text-Modifier/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "c4fda1f42d1186726c67",
+    "url": "/Text-Modifier/static/js/main.993292ac.chunk.js"
+  },
+  {
+    "revision": "1a62e9aa5abf4eb0c507",
+    "url": "/Text-Modifier/static/js/runtime~main.dcce0002.js"
+  }
+]);
